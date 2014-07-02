1fd9f0a5f345fd3f6e319e6cf0b1aa83c757065b
Troy Phillips <tdphillips88@gmail.com>
2014-07-01 21:22:24 -0400

    Make campaigns sortable if vote_type is set to ranking

--- static/prospectus.js
+++ static/prospectus.js
@@ -1,14 +1,14 @@
 $(function() {
-        $('#add-another').click(function(e) {
-                console.log('clicked');
-                e.preventDefault();
-                console.log('clicked; current count: ' + form_count);
-                form_count++;
-                console.log('new count: ' + form_count);
-                var new_form = form.replace(/__prefix__/g, form_count);
-                $('#prospectus-table').append(new_form)
-                $('#id_form-TOTAL_FORMS').val(form_count);
-            }
-        )
-    }
-)
+    $('#add-another').click(function(e) {
+        e.preventDefault();
+        form_count++;
+        var new_form = form.replace(/__prefix__/g, form_count);
+        $('#prospectus-table').append(new_form)
+        $('#id_form-TOTAL_FORMS').val(form_count);
+    });
+
+    $('#vote').click(function(e) {
+        e.preventDefault();
+        /* TODO: Do save voting logic */
+    })
+});
